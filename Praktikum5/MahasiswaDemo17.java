f2f3e2e1b2e5653f9e76ce441a8bf2ec6de39bed
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2026-05-07 08:57:59 +0700

    Sedikit Perbaikan

--- Praktikum5/MahasiswaDemo17.java
+++ Praktikum5/MahasiswaDemo17.java
@@ -15,17 +15,40 @@ public class MahasiswaDemo17 {
         System.out.println("\nData mahasiswa sebelum sorting:");
         list.tampil();
 
-        System.out.println("\nData mahasiswa setelah sorting berdasarkan IPK (DESC):");
-        list.bubbleSort();
-        list.tampil();
+        // System.out.println("\nData mahasiswa setelah sorting berdasarkan IPK
+        // (DESC):");
+        // list.bubbleSort();
+        // list.tampil();
 
         System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
         list.selectionSort();
         list.tampil();
 
-        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
-        list.insertionSort();
-        list.tampil();
+        // System.out.println("Data yang sudah terurut menggunakan INSERTION SORT
+        // (ASC)");
+        // list.insertionSort();
+        // list.tampil();
+
+        System.out.println("---------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("---------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = sc.nextDouble();
+
+        // System.out.println("menggunakan sequential searching");
+        // double posisi = list.sequentialSearching(cari);
+        // int pss = (int) posisi;
+        // list.tampilPosisi(cari, pss);
+        // list.tampilDataSearch(cari, pss);
+        System.out.println("");
+        System.out.println("menggunakan binary search");
+        System.out.println("");
+        int posisi2 = list.findBinarySearch(cari, 0, list.listMhs.length - 1);
+        int pss2 = (int) posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
+
         sc.close();
     }
 }
