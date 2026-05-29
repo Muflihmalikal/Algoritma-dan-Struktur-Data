ea57456cb1f67751474449e7464d95f84e354eca
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2026-05-29 22:59:12 +0700

    Praktikum 2

---
+++ Pertemuan14/BinaryTreeArray17.java
@@ -0,0 +1,28 @@
+package Pertemuan14;
+
+public class BinaryTreeArray17 {
+    Mahasiswa17[] dataMahasiswa;
+    int idxLast;
+
+    public BinaryTreeArray17() {
+        this.dataMahasiswa = new Mahasiswa17[10];
+        this.idxLast = -1; // Set default kosong
+    }
+
+    void populateData(Mahasiswa17[] dataMhs, int idxLast) {
+        this.dataMahasiswa = dataMhs;
+        this.idxLast = idxLast;
+    }
+
+    void traverseInOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            if (dataMahasiswa[idxStart] != null) {
+                traverseInOrder(2 * idxStart + 1);
+                dataMahasiswa[idxStart].tampilInformasi();
+                traverseInOrder(2 * idxStart + 2);
+            }
+        }
+    }
+
+
+}
