95153cd6e8c2e0c98db77a3b7e757a2dc233b928
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2026-06-12 09:44:02 +0700

    Pertemuan 14

---
+++ Praktikum14/SortingDemo.java
@@ -0,0 +1,24 @@
+package Praktikum14;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortingDemo {
+    public static void main(String[] args) {
+        ArrayList<String> daftarSiswa = new ArrayList<>();
+        daftarSiswa.add("Zainab");
+        daftarSiswa.add("Andi");
+        daftarSiswa.add("Rara");
+
+        Collections.sort(daftarSiswa);
+        System.out.println("Data Siswa (Sorted): " + daftarSiswa);
+
+        ArrayList<Customer> customers = new ArrayList<>();
+        customers.add(new Customer(1, "Zakia"));
+        customers.add(new Customer(2, "Budi"));
+        customers.add(new Customer(3, "Andi"));
+
+        customers.sort((c1, c2) -> c1.name.compareTo(c2.name));
+        System.out.println("Data Customers (Sorted by Name): " + customers);
+    }
+}
