4ed16b1e0119597ed09743edd354c90f59c1ae53
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2026-07-06 01:49:12 +0700

    percobaan 2

--- Jobsheet9/StackTugasMahasiswa17.java
+++ Jobsheet9/StackTugasMahasiswa17.java
@@ -68,4 +68,18 @@ public class StackTugasMahasiswa17 {
     public int getJumlahTugas() {
         return top + 1;
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi17 stackKonv = new StackKonversi17();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stackKonv.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stackKonv.isEmpty()) {
+            biner += stackKonv.pop();
+        }
+        return biner;
+    }
 }
