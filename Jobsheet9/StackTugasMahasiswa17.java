10ca1e851d2b7116f850af6ea445137cfd5188c6
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2026-07-06 01:40:18 +0700

    Percobaan 1

---
+++ Jobsheet9/StackTugasMahasiswa17.java
@@ -0,0 +1,71 @@
+package Jobsheet9;
+
+public class StackTugasMahasiswa17 {
+    Mahasiswa17[] stack;
+    int size;
+    int top;
+
+    public StackTugasMahasiswa17(int size) {
+        this.size = size;
+        stack = new Mahasiswa17[size];
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
+
+    public void push(Mahasiswa17 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa17 pop() {
+        if (!isEmpty()) {
+            Mahasiswa17 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa17 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+    
+    // Melihat tugas terbawah (yang pertama kumpul)
+    public Mahasiswa17 peekBottom() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    public void print() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+    }
+    
+    // Menghitung jumlah tugas
+    public int getJumlahTugas() {
+        return top + 1;
+    }
+}
